180dd50c58d7a31d010e39f6e329378d400b0d91
JewSaten <satenjew@gmail.com>
2020-09-28 15:56:54 +0800

    ft

---
+++ mylibrary/src/main/java/com/example/mylibrary/Main.java
@@ -0,0 +1,18 @@
+package com.example.mylibrary;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+public class Main extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Toast.makeText(this,"mylib",Toast.LENGTH_LONG).show();
+    }
+
+
+}
